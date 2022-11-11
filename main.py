e751c33ca0a636c527adc8a75452ae3332f12ee6
afanasiev-d <117452244+afanasiev-d@users.noreply.github.com>
2022-11-11 08:06:44 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-#import matplotlib.pyplot as plt
-#from io import BytesIO
-#import seaborn as sn
 
 import preprocessing
 import binning
