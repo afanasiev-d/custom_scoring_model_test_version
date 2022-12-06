0114b49263299fb971483d861d93370f1dfd7cd0
afanasiev-d <117452244+afanasiev-d@users.noreply.github.com>
2022-12-06 05:38:14 -0500

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -157,3 +157,59 @@ def missing_rate(df):
     df_missing_rate['Missing rate']=((df.isna().sum()/len(df)).round(4) * 100).values
     
     return df_missing_rate
+
+# Calculate information value
+def calc_iv(df, feature, target, pr=False):
+    """
+    Set pr=True to enable printing of output.
+    
+    Output: 
+      * iv: float,
+      * data: pandas.DataFrame
+    """
+
+    lst = []
+
+    df[feature] = df[feature].fillna("NULL")
+
+    for i in range(df[feature].nunique()):
+        val = list(df[feature].unique())[i]
+        lst.append([feature,                                                        # Variable
+                    val,                                                            # Value
+                    df[df[feature] == val].count()[feature],                        # All
+                    df[(df[feature] == val) & (df[target] == 0)].count()[feature],  # Good (think: Fraud == 0)
+                    df[(df[feature] == val) & (df[target] == 1)].count()[feature]]) # Bad (think: Fraud == 1)
+
+    data = pd.DataFrame(lst, columns=['Variable', 'Value', 'All', 'Good', 'Bad'])
+
+    data['Share'] = data['All'] / data['All'].sum()
+    data['Bad Rate'] = data['Bad'] / data['All']
+    data['Distribution Good'] = (data['All'] - data['Bad']) / (data['All'].sum() - data['Bad'].sum())
+    data['Distribution Bad'] = data['Bad'] / data['Bad'].sum()
+    data['WoE'] = np.log(data['Distribution Good'] / data['Distribution Bad'])
+
+    data = data.replace({'WoE': {np.inf: 0, -np.inf: 0}})
+
+    data['IV'] = data['WoE'] * (data['Distribution Good'] - data['Distribution Bad'])
+
+    data = data.sort_values(by=['Variable', 'Value'], ascending=[True, True])
+    data.index = range(len(data.index))
+
+    if pr:
+        print(data)
+        print('IV = ', data['IV'].sum())
+
+
+    iv = data['IV'].sum()
+    # print(iv)
+
+    return iv, data
+
+def get_init_iv(df, target):
+    dictionary_feature_iv={}
+    for feat in df.columns:
+        if feat!=target:
+            dictionary_feature_iv[feat]=calc_iv(df, feat, target, pr=False)[0]
+    df_iv=pd.DataFrame.from_dict(dictionary_feature_iv, orient='index', columns=['IV'])
+    
+    return df_iv
